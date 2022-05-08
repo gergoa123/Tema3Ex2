bda14953325ca0611f7ce2f3cc6fa857cc79baff
Gergoa123 <gergo3124@gmail.com>
2022-05-08 14:31:47 +0300

    Add project files.

---
+++ Tema3Ex2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Tema3Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti numarul:");
+            int numar = int.Parse(Console.ReadLine());
+            int numarBinar = 0;
+            int aux;
+            int i = 1;
+
+            while (numar != 0)
+            {
+                aux = numar % 2;
+                numarBinar = numarBinar + aux * i;
+                i *= 10;
+                numar = numar / 2;
+            }
+
+            Console.WriteLine(numarBinar);
+        }
+    }
+}
